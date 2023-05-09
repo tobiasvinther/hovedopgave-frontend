774f068ad26302729ff2fade6d993a57894ee05f
JonatanSegal <82033910+JonatanSegal@users.noreply.github.com>
2023-05-09 10:39:16 +0200

    Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
+import { Button } from '@mui/material';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button>Test</Button>
       </header>
     </div>
   );
