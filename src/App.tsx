3337a83fc5e3151b4b2fb31c86d3a2a6e0e2d133
JonatanSegal <82033910+JonatanSegal@users.noreply.github.com>
2023-05-09 11:18:38 +0200

    removed strictmode, simple useEffect hook, removed logo from manifest

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import { Button } from '@mui/material';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    fetch(`http://localhost:8080/`)
+     .then((response) => response.json())
+     .then((actualData) => console.log(actualData))
+     .catch((err) => {
+      console.log(err.message);
+     });
+   }, [])
   return (
     <div className="App">
       <header className="App-header">
