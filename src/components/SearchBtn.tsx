69c525c9a7937f772cb32e65a0833558e473c666
Thony <thon0101@stud.kea.dk>
2023-05-17 12:43:19 +0200

    created searchfield

---
+++ src/components/SearchBtn.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
+export default function SearchBtn() {
+  return (
+    <Stack direction="row" spacing={2}>
+      <Button>Søg</Button>
+    </Stack>
+  );
+}
