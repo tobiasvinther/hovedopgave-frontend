fe6bb22c282b6c6312a65ae1431042d2b72d706a
JonatanSegal <82033910+JonatanSegal@users.noreply.github.com>
2023-05-24 03:38:21 +0200

    Date should be fixed now

--- src/pages/observation/observation.tsx
+++ src/pages/observation/observation.tsx
@@ -1,14 +1,14 @@
-import { Button, Stack, TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import { Formik, Form } from "formik";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import dayjs from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
 
 interface Values {
   species: string;
   latitude: string;
   longitude: string;
-  date: string;
+  date: Dayjs;
   note: string;
 }
 interface ObservationProps {
@@ -17,64 +17,79 @@ interface ObservationProps {
 
 export const Observation: React.FC<ObservationProps> = ({ onSubmit }) => {
   return (
-    <Stack alignContent="center" justifyItems="center">
+    <div style={{ textAlign: "center" }}>
       <Formik
         initialValues={{
           species: "",
           latitude: "",
           longitude: "",
-          date: dayjs(Date.now()).toString(),
+          date: dayjs(Date.now()),
           note: "",
         }}
         onSubmit={(values) => onSubmit(values)}
       >
         {({ values, handleChange, setFieldValue }) => (
           <Form>
-            <TextField
-              required
-              label="Hvilken fugl har du set"
-              value={values.species}
-              name="species"
-              onChange={handleChange}
-            />
-            <TextField
-              name="latitude"
-              sx={{ marginBottom: 5, marginTop: 5, width: 300 }}
-              required
-              value={values.latitude}
-              onChange={handleChange}
-            />
-            <TextField
-              name="longitude"
-              sx={{ marginBottom: 5, width: 300 }}
-              required
-              value={values.longitude}
-              onChange={handleChange}
-            />
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DatePicker
-                sx={{ marginBottom: 5, width: 300 }}
-                format="DD/MM/YYYY"
-                label="Dato"
-                onChange={(newValue) => setFieldValue("date", newValue, true)}
-                value={values.date}
+            <div>
+              <TextField
+                sx={{ marginBottom: 5, marginTop: 5, width: 300 }}
+                required
+                label="Hvilken fugl har du set"
+                value={values.species}
+                name="species"
+                onChange={handleChange}
               />
-            </LocalizationProvider>
-            <TextField
-              name="note"
-              sx={{ marginBottom: 5, width: 300 }}
-              multiline
-              rows={4}
-              value={values.note}
-              onChange={handleChange}
-            />
+            </div>
+            <div>
+              <TextField
+                name="latitude"
+                sx={{ marginBottom: 5, width: 300 }}
+                required
+                value={values.latitude}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <TextField
+                name="longitude"
+                sx={{ marginBottom: 5, width: 300 }}
+                required
+                value={values.longitude}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  sx={{ marginBottom: 5, width: 300 }}
+                  format="DD/MM/YYYY"
+                  label="Dato"
+                  onChange={(newValue) => setFieldValue("date", newValue, true)}
+                  value={values.date}
+                />
+              </LocalizationProvider>
+            </div>
+            <div>
+              <TextField
+                name="note"
+                sx={{ marginBottom: 5, width: 300 }}
+                multiline
+                rows={4}
+                value={values.note}
+                onChange={handleChange}
+              />
+            </div>
             <pre>{JSON.stringify(values, null, 2)}</pre>
-            <Button variant="contained" type="submit">
+            <Button
+              variant="contained"
+              type="submit"
+              sx={{ marginBottom: 5, width: 300 }}
+            >
               Submit
             </Button>
           </Form>
         )}
       </Formik>
-    </Stack>
+    </div>
   );
 };
