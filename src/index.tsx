7fede33b9a9755230c76f7e0f38744dcf4bb5083
Thony <thon0101@stud.kea.dk>
2023-06-04 20:46:28 +0200

    register-login-nortification-created

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -10,6 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <BrowserRouter>
     <App />
+    <ToastContainer />
   </BrowserRouter>
 );
 
